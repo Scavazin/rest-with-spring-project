e618ff9d0d493b4dcd4c9c8e98be17a2802fcdd0
Victor Scavazin <victor.scavazin@marsaude.net>
2022-07-24 17:54:39 -0300

    get

--- FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/PersonController.kt
+++ FirstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/PersonController.kt
@@ -3,7 +3,6 @@ package br.com.erudio
 import br.com.erudio.exception.UnsuportedMathOperationException
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
